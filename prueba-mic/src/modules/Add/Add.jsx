5db398ee3256a3e0f9459917c864efda5c5bc8c1
Cristian Rendon <cristianrendon1704@gmail.com>
2023-03-12 16:53:20 -0500

    Verison 1 del add, aun no lo acabo

--- prueba-mic/src/modules/Add/Add.jsx
+++ prueba-mic/src/modules/Add/Add.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import "./Add.css";
 
 const Add = () => {
 
+    const Data = require("../Carousel/Data.json"); //se trae la data del .json
+    const {id,licencia,ref,name,description,maker,value,discount,careful} = Data[0]; //desestrucuracion de datos
+    const [Cuadrado,setCuadrado] = React.useState("S");
+
+
     return (
         <>
-            <div className='Box-34'>
-                VAGÓN-BAR Mucho texto No entendía lo que quería decirme, así que se lo advertí y volvió a intentarlo. Y seguí sin entender. Quizá pensó en un problema de entendederas mías o de explicaderas suyas, pero en realidad ocurría que sus palabras me llegaban como sonidos inarticulados, informes. Me llegaban deformadas e irreconocibles, extenuadas por el esfuerzo de atravesar la tela negra de la mascarilla. Borrosas, diría un amigo propenso a la sinestesia.  Así que renunció a la conversación y la zanjó con un «Mucho texto», que me hizo reír. Significaba que pretendía explicar algo demasiado largo y complejo y que no valía la pena. Estaba muy cansada. Le dije que usaría lo de «Mucho texto», si no le importaba. La honradez intelectual le obligó a aclararme que la expresión no le pertenecía: provenía de un meme de Twitter que se había convertido también en una pegatina (sticker) de WhatsApp. Lo busqué.
-                Consiste en el Yoda de La guerra de las galaxias con sombrero, gafas de sol, anillo, pendiente en el lóbulo de su gigantesca oreja derecha, zapatillas deportivas y otra indumentaria que confieren al venerado maestro verde un aire hípster. La leyenda «Mucho texto» en letras de molde blancas atraviesa la cintura del personaje.  Comprobé que se usa en las redes sociales desde hace un año, más o menos.
+            <div className='Container-add'>
+                <div><h3>{name}</h3></div>
+                <div className="container-ref correr-arriba-30px"><h5>{licencia}</h5> <h6 className="correr-izq-ref">{ref}{Cuadrado}</h6></div>
+                <div className="correr-izq-ref correr-arriba-30px"><h4>${value}</h4></div>
+                <div className="Cuadrado">
+                    <button className={`${Cuadrado === "S" ? 'Cuadado-pintado' : 'Cuadado-NO-Pintado'}`} onClick={()=>{setCuadrado("S")}} >S</button>
+                    <button className={`${Cuadrado === "M" ? 'Cuadado-pintado' : 'Cuadado-NO-Pintado'}`} onClick={()=>{setCuadrado("M")}} >M</button>
+                    <button className={`${Cuadrado === "L" ? 'Cuadado-pintado' : 'Cuadado-NO-Pintado'}`} onClick={()=>{setCuadrado("L")}} >L</button>
+                    <button className={`${Cuadrado === "XL" ? 'Cuadado-pintado' : 'Cuadado-NO-Pintado'}`} onClick={()=>{setCuadrado("XL")}} >XL</button>
+                </div>
+                <div></div>
             </div>
         </>
     )
