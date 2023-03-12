9cd24701295c39b3b0047d1bbc8e6d830c40704e
Cristian Rendon <cristianrendon1704@gmail.com>
2023-03-12 14:51:25 -0500

    Correcciones antes de iniciar add

--- prueba-mic/src/App.js
+++ prueba-mic/src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 import Navbar from './modules/Navbar/Navbar.jsx';
 import Carousel from './modules/Carousel/Carousel.jsx';
+import Add from './modules/Add/Add';
 function App() {
 
-  return(
+  return (
     <>
-    <Navbar></Navbar>
-    <div className="Box-collaider-navbar"></div>
-    <Carousel></Carousel> 
+      <Navbar></Navbar>
+      <div className="Box-collaider-navbar"></div>
+      <div className='Container'>
+        <div className='container-width-60'><Carousel></Carousel> </div>
+        <div className='container-width-40'><Add></Add></div>
+      </div>
+
+
+      {/*funcionalidad add car esta en Carousel en un contenedor  */}
     </>
   )
 }
