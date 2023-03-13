3d87e9b393590d1e0dfe51c9e5577c26eebae29e
Cristian Rendon <cristianrendon1704@gmail.com>
2023-03-12 22:33:42 -0500

    Cambios finales y footer listo

---
+++ prueba-mic/src/modules/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () =>{
+    return(
+        <>
+        <div className="Container-general-footer">
+            <div className="box-footer">
+                <br/> 
+                <h2>Prueba Tecnica MIC</h2>
+                <h3>Hecha por: Cristian Rendon Rodriguez</h3>
+                <h3>cristianrendon1704@gmail.com - +57 3204316408</h3>
+                <br/> 
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Footer;
